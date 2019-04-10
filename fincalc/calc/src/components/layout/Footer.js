dbdd617a3ff7bf86c9879645992f46b49fd68e9f
Twistsmyth <web@skri.biz>
2019-04-10 07:35:42 +0200

    initial - prototype

---
+++ fincalc/calc/src/components/layout/Footer.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+import Axios from 'axios';
+
+export class Footer extends Component {
+
+  render() {
+    return (
+        <div className="col-xs-1 text-center">
+        <br/>
+      </div>
+    );
+  }
+}
+
+export default Footer
